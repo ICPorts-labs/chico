2a6d71635926d1f1ee1149fdee2f0be436a8cc6e
Hassen <hassen.saidi@gmail.com>
2022-06-13 17:58:05 -0700

    Remove the copying of the .did file

--- examples/HelloWorld/run.sh
+++ examples/HelloWorld/run.sh
@@ -14,7 +14,6 @@ dfx canister create helloworld
 dfx build 
 dfx deploy
 cp ../helloworld.wasm .dfx/local/canisters/helloworld/helloworld.wasm
-cp ../helloworld.did .dfx/local/canisters/helloworld/helloworld.did
 dfx canister install -m reinstall helloworld
 dfx canister call helloworld greet "ICP Fan"#!/bin/bash
 
