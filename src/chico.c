14e0620785c96541e8ee458df30a17884d7ddeb3
vagrant <vagrant@vagrant.vm>
2022-07-08 06:53:39 +0000

    add functions to return int/nat 8,16,32, and 64 from a canister

--- src/chico.c
+++ src/chico.c
@@ -9,26 +9,43 @@ Chico: C/C++ CDK for building canisters on the Internet Computer
 #include <string.h>
 #include "ic0.h"
 
+
+//
+// This is the list of supported types.
+// That is, these are the types that can be to and from a C/C++ canister
+// 
+
+
 uint8_t IDL_MAGIC[4] = {0x44, 0x49, 0x44, 0x4C};
-uint8_t IDL_TYPE_int = 0x7C;
+
+//uint8_t IDL_TYPE_NULL = 0x7F;
+uint8_t IDL_TYPE_BOOL = 0x7E;
+// [IDL_MAGIC][00][01][IDL_TYPE_BOOL][00] for false
+// [IDL_MAGIC][00][01][IDL_TYPE_BOOL][01] for true
+// uint8_t IDL_TYPE_NAT = 0x7D;
+// uint8_t IDL_TYPE_int = 0x7C;
+uint8_t IDL_TYPE_NAT8 = 0x7B;
+uint8_t IDL_TYPE_NAT16 = 0x7A;
+uint8_t IDL_TYPE_NAT32 = 0x79;
+uint8_t IDL_TYPE_NAT64 = 0x78;
+uint8_t IDL_TYPE_INT8 = 0x77;
+uint8_t IDL_TYPE_INT16 = 0x76;
+uint8_t IDL_TYPE_INT32 = 0x75;
+uint8_t IDL_TYPE_INT64 = 0x74;
+uint8_t IDL_TYPE_FLOAT32 = 0x73;
+uint8_t IDL_TYPE_FLOAT64 = 0x72;
 uint8_t IDL_TYPE_text = 0x71;
+//uint8_t IDL_TYPE_RESERVED = 0x70;
+//uint8_t IDL_TYPE_EMPTY = 0x6F;
+uint8_t IDL_TYPE_VEC = 0x6D;
+uint8_t IDL_TYPE_RECORD = 0x6C;
+uint8_t IDL_TYPE_PRINCIPAL = 0x68;
 
-void write_magic() {
-  ic0_msg_reply_data_append((uint32_t)(IDL_MAGIC), 4);
-}
 
-void write_byte(uint8_t byte) {
-  ic0_msg_reply_data_append((uint32_t)(&byte), 1);
-}
 
-void write_return_byte(int x) {
-  write_magic();
-  write_byte(0x00);
-  write_byte(0x01);
-  write_byte(IDL_TYPE_int);
-  write_byte((uint32_t)x);
-  ic0_msg_reply();
-}
+//
+// Utility functions for encoding and decoding
+//
 
 void write_uleb128 (uint32_t x) {
   unsigned char buf[10];
@@ -43,9 +60,103 @@ void write_uleb128 (uint32_t x) {
   if ( len >= 256) {
     write_byte((uint32_t)buf[1]);
   }
-  // we assume we don't need more than 256^2 sized buffers
+  // we assume we don't need more than 256^2 sized buffers   
 }
 
+
+//
+// Logging functions
+//
+
+void ic_log_message(const char *message) {
+  ic0_debug_print((uint32_t)message, strlen(message));
+}
+
+//
+//  Functions to read (input) data into a canister canister
+//
+
+void match_byte(const uint8_t *buf, size_t len, int off, uint8_t byte) {
+  if (off >= len || buf[off] != byte) {
+    trap("Invalid byte!");
+  }
+}
+
+void match_magic(const uint8_t *buf, size_t len) {
+  for (int i = 0; i < 4; i++) {
+    match_byte(buf, len, i, IDL_MAGIC[i]);
+  }
+}
+
+//
+//  Functions to write (return) data from a canister
+//
+
+
+void write_magic() {
+  ic0_msg_reply_data_append((uint32_t)(IDL_MAGIC), 4);
+}
+
+void write_byte(uint8_t byte) {
+  ic0_msg_reply_data_append((uint32_t)(&byte), 1);
+ç}
+
+void write_nat16(uint16_t  val) {
+  write_byte((uint8_t)val);
+  write_byte((uint8_t)val>>8);
+}
+
+void write_nat32(uint32_t  val) {
+  write_byte((uint8_t)val);
+  write_byte((uint8_t)(val>>8));
+  write_byte((uint8_t)(val>>16));
+  write_byte((uint8_t)(val>>24));
+}
+
+void write_nat64(uint64_t  val) {
+  write_byte((uint8_t)val);
+  write_byte((uint8_t)(val>>8));
+  write_byte((uint8_t)(val>>24));
+  write_byte((uint8_t)(val>>32));
+  write_byte((uint8_t)(val>>40));
+  write_byte((uint8_t)(val>>48));
+  write_byte((uint8_t)(val>>56));
+}
+
+void write_int16(int16_t  val) {
+  write_byte(val & 0xFF);
+  write_byte(val>>8) & 0xFF);
+}
+
+void write_int32(int32_t  val) {
+  write_byte(val & 0xFF);
+  write_byte((val>>8) & 0xFF);
+  write_byte((val>>16) & 0xFF);
+  write_byte((val>>24) & 0xFF);
+}
+
+void write_int64(int64_t  val) {
+  write_byte(val & 0xFF);
+  write_byte((val>>8) & 0xFF);
+  write_byte((val>>16) & 0xFF);
+  write_byte((val>>24) & 0xFF);
+  write_byte((val>>32) & 0xFF);
+  write_byte((val>>40) & 0xFF);
+  write_byte((val>>48) & 0xFF);
+  write_byte((val>>56) & 0xFF);
+}
+
+// should be renamed to ic_writes_byte
+void write_return_byte(int x) {
+  write_magic();
+  write_byte(0x00);
+  write_byte(0x01);
+  write_byte(IDL_TYPE_int);
+  write_byte((uint32_t)x);
+  ic0_msg_reply();
+}
+
+// return a string (text)
 void ic_writes_text(char *result){
   write_magic();
   write_byte(0x00);
@@ -60,18 +171,98 @@ void ic_writes_text(char *result){
   ic0_msg_reply();
 }
 
-void ic_log_message(const char *message) {
-  ic0_debug_print((uint32_t)message, strlen(message));
+// return a boolean value
+void ic_writes_bool(_Bool val){
+  write_magic();
+  write_byte(0x00);
+  write_byte(0x01);
+  write_byte(IDL_TYPE_BOOL);
+  if (val) {
+    write_byte(0x01);
+  }
+  else {
+    write_byte(0x00);
+    }
+  ic0_msg_reply();
 }
 
-void match_byte(const uint8_t *buf, size_t len, int off, uint8_t byte) {
-  if (off >= len || buf[off] != byte) {
-    trap("Invalid byte!");
-  }
+// return int8_t
+void ic_writes_int8(int8_t  val){
+  write_magic();
+  write_byte(0x00);
+  write_byte(0x01);
+  write_byte(IDL_TYPE_INT8);
+  write_byte(val);
+  ic0_msg_reply();
 }
 
-void match_magic(const uint8_t *buf, size_t len) {
-  for (int i = 0; i < 4; i++) {
-    match_byte(buf, len, i, IDL_MAGIC[i]);
-  }
+// return int_16                                                                                                                                                                                                                                                                                                  
+void ic_writes_int16(int16_t  val){
+  write_magic();
+  write_byte(0x00);
+  write_byte(0x01);
+  write_byte(IDL_TYPE_INT16);
+  write_int16(val);
+  ic0_msg_reply();
 }
+
+// return int_32                                                                                                                                                                                                                                                                                                  
+void ic_writes_int32(int32_t  val){
+  write_magic();
+  write_byte(0x00);
+  write_byte(0x01);
+  write_byte(IDL_TYPE_INT32);
+  write_int16(val);
+  ic0_msg_reply();
+}
+
+// return int_64                                                                                                                                                                                                                                                                                                  
+void ic_writes_int64(int64_t  val){
+  write_magic();
+  write_byte(0x00);
+  write_byte(0x01);
+  write_byte(IDL_TYPE_INT64);
+  write_int64(val);
+  ic0_msg_reply();
+}
+
+// return uint8_t                                                                                                                                                                                                                                                                                                    
+void ic_writes_nat8(uint8_t  val){
+  write_magic();
+  write_byte(0x00);
+  write_byte(0x01);
+  write_byte(IDL_TYPE_NAT8);
+  write_byte(val);
+  ic0_msg_reply();
+}
+
+// return uint_16                                                                                                                                                                                                                                                                                                    
+void ic_writes_nat16(uint16_t  val){
+  write_magic();
+  write_byte(0x00);
+  write_byte(0x01);
+  write_byte(IDL_TYPE_NAT16);
+  write_nat16(val);
+  ic0_msg_reply();
+}
+
+// return uint_32                                                                                                                                                                                                                                                                                                    
+void ic_writes_nat32(uint32_t  val){
+  write_magic();
+  write_byte(0x00);
+  write_byte(0x01);
+  write_byte(IDL_TYPE_NAT32);
+  write_nat32(val);
+  ic0_msg_reply();
+}
+
+// return uint_64                                                                                                                                                                                                                                                                                                    
+void ic_writes_nat64(uint64_t  val){
+  write_magic();
+  write_byte(0x00);
+  write_byte(0x01);
+  write_byte(IDL_TYPE_NAT64);
+  write_nat64(val);
+  ic0_msg_reply();
+}
+
