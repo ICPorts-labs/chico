c101077951f74ac456444c6e8b0f4e3415a67d43
Hassen <hassen.saidi@sri.com>
2022-07-13 02:02:54 -0700

    add tests for nat

--- tests/tests.c
+++ tests/tests.c
@@ -12,7 +12,7 @@ TESTS
 
 
 /*
-Tests
+Tests int
  */
 
 void test_int8() WASM_EXPORT("canister_update test_int8");
@@ -41,4 +41,36 @@ void test_int64() WASM_EXPORT("canister_update test_int64");
 void test_int64() {
   int64_t val = ic_reads_int64();
   ic_writes_int64(val);
+}
+
+/*
+Tests nat
+ */
+
+void test_nat8() WASM_EXPORT("canister_update test_nat8");
+void test_nat8() {
+  ic_log_message("start test");
+  uint8_t val = ic_reads_nat8();
+  ic_log_message("value is read");
+  ic_writes_nat8(val);
+  ic_log_message("end of test");
+}
+
+
+void test_nat16() WASM_EXPORT("canister_update test_nat16");
+void test_nat16() {
+  uint16_t val = ic_reads_nat16();
+  ic_writes_nat16(val);
+}
+
+void test_nat32() WASM_EXPORT("canister_update test_nat32");
+void test_nat32() {
+  uint32_t val = ic_reads_nat32();
+  ic_writes_nat32(val);
+}
+
+void test_int64() WASM_EXPORT("canister_update test_nat64");
+void test_int64() {
+  uint64_t val = ic_reads_nat64();
+  ic_writes_nat64(val);
 }
