7c9158db129a8b7f934dd7b47c2c4e008f1269b4
Hassen <hassen.saidi@sri.com>
2022-07-13 09:10:00 -0700

    add explicit type for arguments when calling dfx

--- tests/run.sh
+++ tests/run.sh
@@ -19,13 +19,13 @@ echo "Canister deployed"
 cp ../tests.wasm .dfx/local/canisters/tests/tests.wasm
 cp ../tests.did .dfx/local/canisters/tests/tests.did
 yes yes | dfx canister install -m reinstall tests
-dfx canister call tests test_int8 '(1)'
-dfx canister call tests test_int8 '(-1)'
-dfx canister call tests test_int16 '(1)'
-dfx canister call tests test_int16 '(-300)'
-dfx canister call tests test_int32 '(300)'
-dfx canister call tests test_int32 '(-1600)'
-dfx canister call tests test_int64 '(300000)'
-dfx canister call tests test_int64 '(-30000000)'
+dfx canister call tests test_int8 '(1)' --type '(int8)'
+dfx canister call tests test_int8 '(-1)' --type '(int8)'
+dfx canister call tests test_int16 '(1)' --type '(int16)'
+dfx canister call tests test_int16 '(-300)' --type '(int16)'
+dfx canister call tests test_int32 '(300)' --type '(int32)'
+dfx canister call tests test_int32 '(-1600)' --type '(int32)'
+dfx canister call tests test_int64 '(300000)' --type '(int64)'
+dfx canister call tests test_int64 '(-30000000)' --type '(int64)'
 
 echo "The end!"
