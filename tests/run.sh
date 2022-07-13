786361c1367c4b51c4409dfa60e80576efbad39c
Hassen <hassen.saidi@sri.com>
2022-07-13 02:09:11 -0700

    add more tests

--- tests/run.sh
+++ tests/run.sh
@@ -20,5 +20,12 @@ cp ../tests.wasm .dfx/local/canisters/tests/tests.wasm
 cp ../tests.did .dfx/local/canisters/tests/tests.did
 yes yes | dfx canister install -m reinstall tests
 dfx canister call tests test_int8 '(1)'
+dfx canister call tests test_int8 '(-1)'
+dfx canister call tests test_int16 '(1)'
+dfx canister call tests test_int16 '(-300)'
+dfx canister call tests test_int32 '(300)'
+dfx canister call tests test_int32 '(-1600)'
+dfx canister call tests test_int64 '(300000)'
+dfx canister call tests test_int64 '(-30000000)'
 
 echo "The end!"
