5fd656cd5d15005a9d700716d9de4495c90069f2
Hassen <hassen.saidi@sri.com>
2022-07-13 01:22:35 -0700

    Use utils.o as it contains the definition of trap which needs to be moved.

--- tests/build.sh
+++ tests/build.sh
@@ -14,13 +14,17 @@ echo ${SYSROOT}
 clang -c ${CHICO_PATH}/chico.c -o chico.o \
       -I ${CHICO_PATH} \
       -O3 --sysroot=${SYSROOT} --target=wasm32-wasi
+      
+clang -c ${CHICO_PATH}/utils.c -o utils.o \
+      -I ${CHICO_PATH} \
+      -O3 --sysroot=${SYSROOT} --target=wasm32-wasi
 
 clang -c tests.c -o tests.o \
       -I ${CHICO_PATH} \
       -O3 --sysroot=${SYSROOT} --target=wasm32-wasi
 
 
-wasm-ld tests.o chico.o -o tests.wasm \
+wasm-ld tests.o chico.o utils.o -o tests.wasm \
 	--demangle --allow-undefined --export-dynamic --no-entry  \
 	${WASI_LIBC}
 
