2160f07a001dc09ecac33ec238d8880507b3c916
Hassen <hassen.saidi@sri.com>
2022-07-13 01:58:09 -0700

    add more functions to chico.h

--- src/chico.h
+++ src/chico.h
@@ -21,3 +21,11 @@ void match_magic(const uint8_t *buf, size_t len);
 void ic_writes_text(char *result);
 
 void ic_writes_int8(int8_t  val);
+void ic_writes_int16(int16_t  val);
+void ic_writes_int32(int32_t  val);
+void ic_writes_int64(int64_t  val);
+
+void ic_writes_nat8(uint8_t  val);
+void ic_writes_nat16(uint16_t  val);
+void ic_writes_nat32(uint32_t  val);
+void ic_writes_nat64(uint64_t  val);
