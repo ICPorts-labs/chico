a5014a92433060bafcbd30880b8912c39407f9d5
Hassen <hassen.saidi@sri.com>
2022-07-20 22:01:53 -0700

    remove log messages in tests

--- tests/tests.c
+++ tests/tests.c
@@ -18,39 +18,27 @@ Tests int
 
 void test_int8() WASM_EXPORT("canister_update test_int8");
 void test_int8() {
-  ic_log_message("start test");
   int8_t val = ic_reads_int8();
-  ic_log_message("value is read");
   ic_writes_int8(val);
-  ic_log_message("end of test");
 }
 
 
 void test_int16() WASM_EXPORT("canister_update test_int16");
 void test_int16() {
-  ic_log_message("start test");
   int16_t val = ic_reads_int16();
-  ic_log_message("value is read");
   ic_writes_int16(val);
-  ic_log_message("end of test");
 }
 
 void test_int32() WASM_EXPORT("canister_update test_int32");
 void test_int32() {
-  ic_log_message("start test");
   int32_t val = ic_reads_int32();
-  ic_log_message("value is read");
   ic_writes_int32(val);
-  ic_log_message("end of test");
 }
 
 void test_int64() WASM_EXPORT("canister_update test_int64");
 void test_int64() {
-  ic_log_message("start test");
   int64_t val = ic_reads_int64();
-  ic_log_message("value is read");
   ic_writes_int64(val);
-  ic_log_message("end of test");
 }
 
 /*
@@ -59,39 +47,27 @@ Tests nat
 
 void test_nat8() WASM_EXPORT("canister_update test_nat8");
 void test_nat8() {
-  ic_log_message("start test");
   uint8_t val = ic_reads_nat8();
-  ic_log_message("value is read");
   ic_writes_nat8(val);
-  ic_log_message("end of test");
 }
 
 
 void test_nat16() WASM_EXPORT("canister_update test_nat16");
 void test_nat16() {
-  ic_log_message("start test");
   uint16_t val = ic_reads_nat16();
-  ic_log_message("value is read");
   ic_writes_nat16(val);
-  ic_log_message("end of test");
 }
 
 void test_nat32() WASM_EXPORT("canister_update test_nat32");
 void test_nat32() {
-  ic_log_message("start test");
   uint32_t val = ic_reads_nat32();
-  ic_log_message("value is read");
   ic_writes_nat32(val);
-  ic_log_message("end of test");
 }
 
 void test_nat64() WASM_EXPORT("canister_update test_nat64");
 void test_nat64() {
-  ic_log_message("start test");
   uint64_t val = ic_reads_nat64();
-  ic_log_message("value is read");
   ic_writes_nat64(val);
-  ic_log_message("end of test");
 }
 
 /*
@@ -164,7 +140,6 @@ void test_vec_nat8() {
 
 void test_text() WASM_EXPORT("canister_update test_text");
 void test_text() {
-  ic_log_message("test text");
   char *val= ic_reads_text();
   ic_writes_text(val);
 }
