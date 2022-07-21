e68715d0f5f745505f7fcd5c6d4518523a5e96ce
Hassen <hassen.saidi@sri.com>
2022-07-21 16:33:47 -0700

    update hello world test to dfx 11

--- examples/HelloWorld/run.sh
+++ examples/HelloWorld/run.sh
@@ -4,20 +4,18 @@
 # Build HelloWorld canister and start alocal replica in the background
 #
 
-dfx new helloworld
+dfx new helloworld --frontend 
 cd helloworld
 dfx start --background 
 
-dfx canister create helloworld_assets
-dfx canister create helloworld
-
+dfx canister create helloworld_backend
 dfx build 
 dfx deploy
 
 echo "Canister deployed"
 
-cp ../helloworld.wasm .dfx/local/canisters/helloworld/helloworld.wasm
-yes yes | dfx canister install -m reinstall helloworld
-dfx canister call helloworld greet "$1"
+cp ../helloworld.wasm .dfx/local/canisters/helloworld_backend/helloworld_backend.wasm
+yes yes | dfx canister install -m reinstall helloworld_backend
+dfx canister call helloworld_backend greet "$1"
 
 echo "The end!"
