7022e50fb961e1762a9955a8fe477de1ba7de3fc
Hassen <hassen.saidi@sri.com>
2022-07-21 21:54:57 -0700

    add chico.o to the wasm build

--- examples/SQLite/build.sh
+++ examples/SQLite/build.sh
@@ -53,7 +53,7 @@ clang -c sqlite3-canister.c  -o sqlite3-canister.o \
       -I ${CHICO_PATH} \
       -I.
 
-wasm-ld sqlite3-canister.o memvfs.o sqlite_os_init.o sqlite3.o unreachable.o utils.o -o sqlite.wasm \
+wasm-ld sqlite3-canister.o memvfs.o sqlite_os_init.o sqlite3.o chico.o unreachable.o utils.o -o sqlite.wasm \
 	--demangle --allow-undefined --export-dynamic --no-entry  \
 	${WASI_LIBC}
 
