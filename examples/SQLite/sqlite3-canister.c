64b1890a56dba9d9fbddccdaaafd4f5db27814b8
Hassen <hassen.saidi@sri.com>
2022-07-21 22:18:49 -0700

    make use of ic_reads_text in SQLite

--- examples/SQLite/sqlite3-canister.c
+++ examples/SQLite/sqlite3-canister.c
@@ -57,24 +57,13 @@ void sqlite_init() {
 // call to update DB
 void sqlite_update() WASM_EXPORT("canister_update sqlite_update");
 void sqlite_update() {
-  size_t len = (size_t)(ic0_msg_arg_data_size());
-  uint8_t *buf = (uint8_t *)(malloc(len));
-  ic0_msg_arg_data_copy((uint32_t)(buf), 0, (uint32_t)(len));
-  match_magic(buf, len);
-  match_byte(buf, len, 4, 0x00);
-  match_byte(buf, len, 5, 0x01);
-  match_byte(buf, len, 6, IDL_TYPE_TEXT);
-  int offset = 0;
-  if (len < 256) {
-    offset =8;
-    }
-  else {
-    offset =9;
-  }
-  ic_log_message(buf+offset);
-  int sql_len = len - offset;
+  char *buf= ic_reads_text();
+  int sql_len = strlen(val);
+  // display update message
+  ic_log_message(buf);
+ 
   sqlite3_str *sql_stmt = sqlite3_str_new(db);
-  sqlite3_str_append(sql_stmt,buf+offset,sql_len );
+  sqlite3_str_append(sql_stmt,buf,sql_len );
   free(buf);
   const char *sqlptr = sqlite3_str_finish(sql_stmt);
   char* messageError=0;
@@ -113,25 +102,14 @@ int write_back(sqlite3_str *query_result_buf , int argc, char **argv,
 // call to query DB
 void sqlite_query() WASM_EXPORT("canister_query sqlite_query");
 void sqlite_query() {
-  size_t len = (size_t)(ic0_msg_arg_data_size());
-  uint8_t *buf = (uint8_t *)(malloc(len));
-  ic0_msg_arg_data_copy((uint32_t)(buf), 0, (uint32_t)(len));
-  match_magic(buf, len);
-  match_byte(buf, len, 4, 0x00);
-  match_byte(buf, len, 5, 0x01);
-  match_byte(buf, len, 6, IDL_TYPE_TEXT);
-  int offset = 0;
-  if (len < 256) {
-    offset =8;
-    }
-  else {
-    offset =9;
-  }
-  ic_log_message(buf+offset);
-  int sql_len = len - offset;
+  char *buf= ic_reads_text();
+  int sql_len = strlen(val);
+  // display update message
+  ic_log_message(buf);
+
   sqlite3_str *sql_stmt = sqlite3_str_new(db);
   sqlite3_str *query_result_buf = sqlite3_str_new(db);
-  sqlite3_str_append(sql_stmt,buf+offset,sql_len );
+  sqlite3_str_append(sql_stmt,buf,sql_len );
   free(buf);
   char *sqlptr = sqlite3_str_finish(sql_stmt);
   char* messageError=0;
